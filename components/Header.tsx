310304e625f30f1c69b34960971f21757652b10e
kou72 <genpokopokopoko@gmail.com>
2023-04-02 09:19:53 +0900

    eslintのコメントでエラー解消

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,6 +16,7 @@ const Header = () => {
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
