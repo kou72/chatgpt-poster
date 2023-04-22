190b78525a2a371be382befd7e7728f69ec2ded6
kou72 <genpokopokopoko@gmail.com>
2023-04-22 19:58:52 +0900

    Layoutコンポーネントにサイドバーのリサイズ機能を追加、_document.tsxでシングルクォーテーションを使うように修正
    components/Layout.tsx
    - useStateフックを使って、sidebarWidthという状態変数を定義
    - handleMouseDown関数を定義し、マウスが押された時にmousemoveとmouseupイベントをリッスンするように設定
    - handleMouseMove関数を定義し、マウスが動いた時にsidebarWidthを更新するように設定
    - handleMouseUp関数を定義し、mousemoveとmouseupイベントのリッスンを解除するように設定
    - サイドバーの幅をstyle属性で設定するように変更
    - サイドバーのリサイズ用のdiv要素を追加し、onMouseDownイベントでhandleMouseDown関数を呼び出すように設定
    
    pages/_document.tsx
    - シングルクォーテーションを使うように修正

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,26 @@
+import { useState } from 'react'
 import Header from './Header'
 import Sidebar from './Sidebar'
 import Head from 'next/head'
 
 export default function Layout({ children }: { children: any }) {
+  const [sidebarWidth, setSidebarWidth] = useState(20)
+
+  const handleMouseDown = (e: { preventDefault: () => void }) => {
+    e.preventDefault()
+    document.addEventListener('mousemove', handleMouseMove)
+    document.addEventListener('mouseup', handleMouseUp)
+  }
+
+  const handleMouseMove = (e: { clientX: number }) => {
+    setSidebarWidth((e.clientX / window.innerWidth) * 100)
+  }
+
+  const handleMouseUp = () => {
+    document.removeEventListener('mousemove', handleMouseMove)
+    document.removeEventListener('mouseup', handleMouseUp)
+  }
+
   return (
     <>
       <Head>
@@ -10,10 +28,14 @@ export default function Layout({ children }: { children: any }) {
       </Head>
       <Header />
       <div className="flex min-h-screen">
-        <div className="w-2/12">
+        <div style={{ width: `${sidebarWidth}%` }}>
           <Sidebar />
         </div>
-        <div className="w-10/12">
+        <div
+          className="bg-gray-300 w-1 cursor-col-resize z-2"
+          onMouseDown={handleMouseDown}
+        ></div>
+        <div className="flex-grow">
           <main>{children}</main>
         </div>
       </div>
