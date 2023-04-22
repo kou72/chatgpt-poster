862045a8ed8f00f45ffa6221b875e1684a6f3e3f
kou72 <genpokopokopoko@gmail.com>
2023-04-22 20:50:40 +0900

    LayoutコンポーネントのPropsを変更し、HeaderとSidebarを別ファイルからインポートするように修正。また、Layout内の表示を変更し、4つのエリアに分割するように変更。index.tsxでLayoutコンポーネントにPropsを渡すように修正し、ChatGPTPosterコンポーネントを削除。
    components/Layout.tsx
    - HeaderとSidebarをインポートするためのimport文を追加
    - LayoutコンポーネントのPropsを変更し、4つのエリアに分割するように修正
    
    pages/index.tsx
    - HeaderとSidebarをインポートするためのimport文を追加
    - ChatGPTPosterコンポーネントを削除
    - LayoutコンポーネントにPropsを渡すように修正

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
-import Header from './Header'
-import Sidebar from './Sidebar'
-import Head from 'next/head'
 
-export const Layout = ({ children }: { children: any }) => {
+interface Props {
+  hedaer: JSX.Element
+  sidebar: JSX.Element
+  topLeft: JSX.Element
+  topRight: JSX.Element
+  bottomLeft: JSX.Element
+  bottomRight: JSX.Element
+}
+
+export const Layout = (props: Props) => {
   const [sidebarWidth, setSidebarWidth] = useState(20)
 
   const handleMouseDown = (e: { preventDefault: () => void }) => {
@@ -23,19 +29,32 @@ export const Layout = ({ children }: { children: any }) => {
 
   return (
     <>
-      <div className="fixed w-full h-12 z-10">
-        <Header />
-      </div>
+      <div className="fixed w-full h-12 z-10">{props.hedaer}</div>
       <div className="flex min-h-screen">
-        <div style={{ width: `${sidebarWidth}%` }}>
-          <Sidebar />
-        </div>
+        <div style={{ width: `${sidebarWidth}%` }}>{props.sidebar}</div>
         <div
           className="bg-gray-300 w-1 cursor-col-resize z-2"
           onMouseDown={handleMouseDown}
         ></div>
         <div className="flex-grow">
-          <main>{children}</main>
+          {/* --- */}
+          <div className="bg-gray-700 min-h-screen pt-12">
+            <div className="flex flex-col h-full">
+              <div className="h-1/12">
+                <div className="grid grid-cols-2 px-2 pt-2 gap-2">
+                  <div className="col-span-1">{props.topLeft}</div>
+                  <div className="col-span-1">{props.topRight}</div>
+                </div>
+              </div>
+              <div className="h-11/12">
+                <div className="grid grid-cols-2 p-2 gap-2 h-[calc(100vh-9rem)]">
+                  <div className="col-span-1">{props.bottomLeft}</div>
+                  <div className="col-span-1">{props.bottomRight}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* --- */}
         </div>
       </div>
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import { UserTextArea } from '@/components/UserTextArea'
 import { PresentChats } from '@/components/PresentChats'
 import { Response } from '@/components/Response'
 import { Layout } from '@/components/Layout'
+import Header from '@/components/Header'
+import Sidebar from '@/components/Sidebar'
 
 const Home = () => {
   return (
@@ -11,39 +13,15 @@ const Home = () => {
       <Head>
         <title>Chat-GPT Poster</title>
       </Head>
-      <Layout>
-        <ChatGPTPoster />
-      </Layout>
+      <Layout
+        hedaer={<Header />}
+        sidebar={<Sidebar />}
+        topLeft={<SystemTextArea />}
+        topRight={<PresentChats />}
+        bottomLeft={<UserTextArea />}
+        bottomRight={<Response />}
+      />
     </>
-  )
-}
-
-const ChatGPTPoster = () => {
-  return (
-    <div className="bg-gray-700 min-h-screen pt-12">
-      <div className="flex flex-col h-full">
-        <div className="h-1/12">
-          <div className="grid grid-cols-2 px-2 pt-2 gap-2">
-            <div className="col-span-1">
-              <SystemTextArea />
-            </div>
-            <div className="col-span-1">
-              <PresentChats />
-            </div>
-          </div>
-        </div>
-        <div className="h-11/12">
-          <div className="grid grid-cols-2 p-2 gap-2 h-[calc(100vh-9rem)]">
-            <div className="col-span-1">
-              <UserTextArea />
-            </div>
-            <div className="col-span-1">
-              <Response />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
   )
 }
 
