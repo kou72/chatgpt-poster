2503aae297d6731e1cf1c3bfc65bbcb0a0971a38
kou72 <genpokopokopoko@gmail.com>
2023-04-24 00:17:22 +0900

    PresentChats.tsxファイルで、チャット画面のテキストエリアの挙動を修正
    PresentChats.tsx
    - useStateフックを使って、isFocusedという状態を管理するように変更
    - handleButtonClick関数を削除し、onFocusHandlerとonBlurHandler関数を追加
    - テキストエリアの高さをisFocusedの状態に応じて変更するように変更
    - テキストエリアの高さをtransitionでアニメーションさせるように変更
    - テキストエリアの高さを1.5remに設定し、フォーカス時にはフルサイズに拡大するように変更
    - テキストエリアを2つ追加し、同様の挙動を実装

--- components/PresentChats.tsx
+++ components/PresentChats.tsx
@@ -2,34 +2,34 @@ import { useState, useRef } from 'react'
 import { useChatGPT } from '../hooks/useChatGPT'
 
 export const PresentChats = () => {
-  const [expanded, setExpanded] = useState(false)
-  const [arrowDirection, setArrowDirection] = useState('down')
-  const textAreaRef = useRef(null)
+  const [isFocused, setIsFocused] = useState(false)
 
-  const handleButtonClick = () => {
-    setExpanded(!expanded)
-    setArrowDirection(arrowDirection === 'down' ? 'up' : 'down')
+  const onFocusHandler = () => {
+    setIsFocused(true)
   }
 
-  const handleBlur = () => {
-    setExpanded(false)
-    setArrowDirection('down')
+  const onBlurHandler = () => {
+    setIsFocused(false)
   }
 
   return (
     <>
-      <button className="w-full" onClick={handleButtonClick}>
-        {arrowDirection === 'down' ? '▼' : '▲'}
-      </button>
-      <div
-        className={`${
-          expanded ? 'h-[calc(100vh-9rem)]' : 'h-0'
-        } overflow-hidden bg-gray-300 w-full absolute top-12 transition-all duration-500 ease-in-out`}
-      >
+      <div className="w-full h-full overflow-y-auto">
         <textarea
-          ref={textAreaRef}
-          className={`bg-gray-300 w-full h-[calc(100vh-9rem)] overflow-y-auto p-1 transition-all duration-500 ease-in-out`}
-          onBlur={handleBlur}
+          className={`bg-gray-300 p-1 resize-none w-full ${
+            isFocused ? 'h-full' : 'h-[1.5rem]'
+          } transition-all`}
+          onFocus={onFocusHandler}
+          onBlur={onBlurHandler}
+          placeholder="ここにテキストを入力してください..."
+        ></textarea>
+        <textarea
+          className={`bg-gray-300 p-1 resize-none w-full ${
+            isFocused ? 'h-full' : 'h-[1.5rem]'
+          } transition-all`}
+          onFocus={onFocusHandler}
+          onBlur={onBlurHandler}
+          placeholder="ここにテキストを入力してください..."
         ></textarea>
       </div>
     </>
