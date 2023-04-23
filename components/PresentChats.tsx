f6657e9751dfe0141a62084788bf2e272c0f494d
kou72 <genpokopokopoko@gmail.com>
2023-04-24 00:23:16 +0900

    PresentChatsコンポーネントのレイアウトを変更し、チャットの表示形式を変更
    - チャットの表示形式を変更するため、textareaを2つからdivとtextareaの組み合わせに変更
    - チャットの送信者を表示するため、div要素を追加
    - チャットの削除ボタンを表示するため、div要素を追加

--- components/PresentChats.tsx
+++ components/PresentChats.tsx
@@ -14,23 +14,39 @@ export const PresentChats = () => {
 
   return (
     <>
-      <div className="w-full h-full overflow-y-auto">
-        <textarea
-          className={`bg-gray-300 p-1 resize-none w-full ${
-            isFocused ? 'h-full' : 'h-[1.5rem]'
-          } transition-all`}
-          onFocus={onFocusHandler}
-          onBlur={onBlurHandler}
-          placeholder="ここにテキストを入力してください..."
-        ></textarea>
-        <textarea
-          className={`bg-gray-300 p-1 resize-none w-full ${
-            isFocused ? 'h-full' : 'h-[1.5rem]'
-          } transition-all`}
-          onFocus={onFocusHandler}
-          onBlur={onBlurHandler}
-          placeholder="ここにテキストを入力してください..."
-        ></textarea>
+      <div className="w-full h-full overflow-y-auto flex flex-col">
+        <div className="h-full flex justify-between items-center p-1">
+          <div className="w-1/12">
+            <p>user</p>
+          </div>
+          <textarea
+            className={`bg-gray-300 p-1 resize-none flex-grow ${
+              isFocused ? 'h-full' : 'h-[1.5rem]'
+            } transition-all`}
+            onFocus={onFocusHandler}
+            onBlur={onBlurHandler}
+            placeholder="ここにテキストを入力してください..."
+          ></textarea>
+          <div className="w-1/12">
+            <p>削除</p>
+          </div>
+        </div>
+        <div className="h-full flex justify-between items-center p-1">
+          <div className="w-1/12">
+            <p>assistant</p>
+          </div>
+          <textarea
+            className={`bg-gray-300 p-1 resize-none flex-grow ${
+              isFocused ? 'h-full' : 'h-[1.5rem]'
+            } transition-all`}
+            onFocus={onFocusHandler}
+            onBlur={onBlurHandler}
+            placeholder="ここにテキストを入力してください..."
+          ></textarea>
+          <div className="w-1/12">
+            <p>削除</p>
+          </div>
+        </div>
       </div>
     </>
   )
