3760c847bf8c42e31cfe00b0b45280f76282fd6a
kou72 <genpokopokopoko@gmail.com>
2023-04-24 13:00:58 +0900

    PresentChatsコンポーネントに、チャットの追加機能を実装
    PresentChats.tsx
    - useStateフックを使って、focusedTextareaとchatsを定義
    - chatsに初期値を設定
    - onFocusHandler関数を修正し、引数で受け取ったindexを使って、focusedTextareaを更新するように変更
    - addChat関数を定義し、chatsに新しいチャットを追加するように変更
    - チャットの表示部分を、chatsをmap関数でループして表示するように変更
    - textareaのvalue属性に、chatsのcontentを設定するように変更
    - 追加ボタンを実装し、クリックしたらaddChat関数を呼び出すように変更

--- components/PresentChats.tsx
+++ components/PresentChats.tsx
@@ -1,53 +1,62 @@
 import { useState, useRef } from 'react'
 import { useChatGPT } from '../hooks/useChatGPT'
 
-export const PresentChats = () => {
-  const [isFocused, setIsFocused] = useState(false)
+import { useState, useRef } from 'react'
 
-  const onFocusHandler = () => {
-    setIsFocused(true)
+export const PresentChats = () => {
+  const [focusedTextarea, setFocusedTextarea] = useState<number | null>(null)
+  const [chats, setChats] = useState([
+    { role: 'user', content: '質問1' },
+    { role: 'assistant', content: '回答1.' },
+    { role: 'user', content: '質問2' },
+    { role: 'assistant', content: '回答2.' },
+  ])
+
+  const onFocusHandler = (index: number) => {
+    setFocusedTextarea(index)
   }
 
   const onBlurHandler = () => {
-    setIsFocused(false)
+    setFocusedTextarea(null)
+  }
+
+  const addChat = () => {
+    setChats([
+      ...chats,
+      { role: 'user', content: '新しい質問' },
+      { role: 'assistant', content: '新しい回答' },
+    ])
   }
 
   return (
     <>
       <div className="w-full h-full overflow-y-auto flex flex-col">
-        <div className={`flex p-1`}>
-          <div className="w-1/12">
-            <p>user</p>
+        {chats.map((chat, index) => (
+          <div key={index} className={`flex p-1`}>
+            <div className="w-1/12">
+              <p>{chat.role}</p>
+            </div>
+            <textarea
+              value={chat.content}
+              className={`bg-gray-300 p-1 resize-none grow transition-all overflow-y-auto ${
+                focusedTextarea === index ? 'h-[12rem]' : 'h-[3rem]'
+              }`}
+              onFocus={() => onFocusHandler(index)}
+              onBlur={onBlurHandler}
+              placeholder="ここにテキストを入力してください..."
+            ></textarea>
+            <div className="w-1/12">
+              <p>削除</p>
+            </div>
           </div>
-          <textarea
-            className={`bg-gray-300 p-1 resize-none grow transition-all overflow-y-auto ${
-              isFocused ? 'h-[12rem]' : 'h-[3rem]'
-            }`}
-            onFocus={onFocusHandler}
-            onBlur={onBlurHandler}
-            placeholder="ここにテキストを入力してください..."
-          ></textarea>
-          <div className="w-1/12">
-            <p>削除</p>
-          </div>
-        </div>
-        <div className={`flex p-1`}>
-          <div className="w-1/12">
-            <p>assistant</p>
-          </div>
-          <textarea
-            className={`bg-gray-300 p-1 resize-none grow transition-all overflow-y-auto ${
-              isFocused ? 'h-[12rem]' : 'h-[3rem]'
-            }`}
-            onFocus={onFocusHandler}
-            onBlur={onBlurHandler}
-            placeholder="ここにテキストを入力してください..."
-          ></textarea>
-          <div className="w-1/12">
-            <p>削除</p>
-          </div>
-        </div>
+        ))}
       </div>
+      <button
+        className="bg-blue-500 text-white px-4 py-2 mt-2 rounded"
+        onClick={addChat}
+      >
+        要素を追加
+      </button>
     </>
   )
 }
