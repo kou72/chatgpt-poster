a4807ea6694409eee00e5dbb18b75591fa9ff6f4
kou72 <genpokopokopoko@gmail.com>
2023-04-24 13:04:35 +0900

    PresentChatsコンポーネントのレイアウトを修正
    - useStateとuseRefをimportしていたが、使用していなかったため削除
    - divタグで囲まれていた部分を削除し、map関数で生成される要素を直接返すように修正
    - ボタンのスタイルを変更

--- components/PresentChats.tsx
+++ components/PresentChats.tsx
@@ -1,8 +1,6 @@
 import { useState, useRef } from 'react'
 import { useChatGPT } from '../hooks/useChatGPT'
 
-import { useState, useRef } from 'react'
-
 export const PresentChats = () => {
   const [focusedTextarea, setFocusedTextarea] = useState<number | null>(null)
   const [chats, setChats] = useState([
@@ -29,34 +27,32 @@ export const PresentChats = () => {
   }
 
   return (
-    <>
-      <div className="w-full h-full overflow-y-auto flex flex-col">
-        {chats.map((chat, index) => (
-          <div key={index} className={`flex p-1`}>
-            <div className="w-1/12">
-              <p>{chat.role}</p>
-            </div>
-            <textarea
-              value={chat.content}
-              className={`bg-gray-300 p-1 resize-none grow transition-all overflow-y-auto ${
-                focusedTextarea === index ? 'h-[12rem]' : 'h-[3rem]'
-              }`}
-              onFocus={() => onFocusHandler(index)}
-              onBlur={onBlurHandler}
-              placeholder="ここにテキストを入力してください..."
-            ></textarea>
-            <div className="w-1/12">
-              <p>削除</p>
-            </div>
+    <div className="w-full h-full overflow-y-auto flex flex-col">
+      {chats.map((chat, index) => (
+        <div key={index} className={`flex p-1`}>
+          <div className="w-1/12">
+            <p>{chat.role}</p>
           </div>
-        ))}
-      </div>
+          <textarea
+            value={chat.content}
+            className={`bg-gray-300 p-1 resize-none grow transition-all overflow-y-auto ${
+              focusedTextarea === index ? 'h-[12rem]' : 'h-[3rem]'
+            }`}
+            onFocus={() => onFocusHandler(index)}
+            onBlur={onBlurHandler}
+            placeholder="ここにテキストを入力してください..."
+          ></textarea>
+          <div className="w-1/12">
+            <p>削除</p>
+          </div>
+        </div>
+      ))}
       <button
-        className="bg-blue-500 text-white px-4 py-2 mt-2 rounded"
+        className="text-white bg-gray-500 px-4 py-2 m-2 rounded-md"
         onClick={addChat}
       >
         要素を追加
       </button>
-    </>
+    </div>
   )
 }
