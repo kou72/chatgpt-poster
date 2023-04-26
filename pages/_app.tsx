c6fff1685c10e1e9122cb7566ada217e82ff5341
kou72 <genpokopokopoko@gmail.com>
2023-04-26 18:57:54 +0900

    @vercel/analyticsを追加し、アプリケーションに統合する
    package.json
    - "@vercel/analytics": "^1.0.0"をdependenciesに追加
    
    package-lock.json
    - "@vercel/analytics": "^1.0.0"の情報を追加
    
    pages/_app.tsx
    - "@vercel/analytics/react"からAnalyticsをインポート
    - Appコンポーネント内に<Analytics />を追加し、アプリケーションに統合

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import '@/styles/globals.css'
 import 'github-markdown-css/github-markdown-dark.css'
 import '@/styles/custom-github-markdown.css'
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
 import { RecoilRoot } from 'recoil'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <Component {...pageProps} />
+      <Analytics />
     </RecoilRoot>
   )
 }
