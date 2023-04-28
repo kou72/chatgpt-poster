779c29e2053c69f0d9c187a739fbb8fddddffa09
kou72 <genpokopokopoko@gmail.com>
2023-04-29 08:05:57 +0900

    Sidebar.tsxから、コメントアウトされたコードを削除
    Sidebar.tsx
    - コメントアウトされたコードを削除

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -66,36 +66,6 @@ const Sidebar = () => {
     )
   }
 
-  // return (
-  //   <div className="bg-gray-600 h-full text-center">
-  //     {/* <div className="p-4">
-  //       <div className="border border-white rounded-md p-4">
-  //         <p className="m-2 text-white">使った金額</p>
-  //         <p className="m-2 text-white text-xl">{usedYen.toFixed(3) ?? 0} 円</p>
-  //         <button
-  //           className="m-2 px-4 py-1 text-white bg-gray-500 rounded-md"
-  //           onClick={resetTotalTokens}
-  //         >
-  //           Reset
-  //         </button>
-  //       </div>
-  //     </div> */}
-  //     {/* <div className="overflow-y-auto">
-  //       <ul className="list-none w-full flex flex-col-reverse">
-  //         {history.map((item, index) => (
-  //           <li
-  //             key={index}
-  //             className="py-2 px-4 m-2 max-h-32 overflow-hidden text-white text-xs text-left border border-white cursor-pointer rounded"
-  //             onClick={() => handleClick(index)}
-  //           >
-  //             {item.input}
-  //           </li>
-  //         ))}
-  //       </ul>
-  //     </div> */}
-  //   </div>
-  // )
-
   return main()
 }
 
