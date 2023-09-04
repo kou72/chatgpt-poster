6d980187dc2990bd238e4ad7ce6ba7efe860056a
kou72 <genpokopokopoko@gmail.com>
2023-09-04 12:03:47 +0900

    _document.tsxファイルを修正し、faviconやOGタグを追加。また、chatgptposter.pngファイルを新規作成。
    _document.tsx
    - Google AdSenseのスクリプトを削除
    + faviconを設定
    + OGタグを追加
      - og:title: ChatGpt Posterというタイトルを設定
      - og:description: ChatGPT APIをWEBから使えるサイトです。プロンプト作りのお供にという説明を設定
      - og:image: chatgptposter.pngという画像を設定
    
    chatgptposter.png
    - 新規作成されたバイナリファイル

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,11 +5,19 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3019938220049179"
-          crossOrigin="anonymous"
-        ></script>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🤖</text></svg>"
+        />
+        <meta property="og:title" content="ChatGpt Poster" />
+        <meta
+          property="og:description"
+          content="ChatGPT APIをWEBから使えるサイトです。プロンプト作りのお供に"
+        />
+        <meta
+          property="og:image"
+          content="https://raw.githubusercontent.com/kou72/chatgpt-poster/main/public/chatgptposter.png"
+        />
       </Head>
       <body>
         <Script src="https://adm.shinobi.jp/s/e53d8d64dbcb566bff8cc21525a343af"></Script>
